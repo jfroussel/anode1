d94186aac021bf95cebc886f5f77c299ec62a956
jeff <jfroussel75@gmail.com>
2017-05-13 22:50:31 +0200

    WIP

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var app = express();
 var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
 
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('layout extractScripts', true);
 app.set('layout extractStyles', true);
 
+app.use(express.static(__dirname + '/public'));
 app.use(expressLayouts);
 
 
@@ -17,8 +17,20 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+app.get('/auth', (req, res) => {
+  res.render('auth');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
 });
 
 app.get('/about', (req, res) => {
@@ -32,8 +44,6 @@ app.get('/documentation', (req, res) => {
 app.get('/post/:id', (req, res) => {
   res.render('post');
 });
-
-
 
 
 var port = 3000;
