ffabb40f4f69839f4345bcc83bc7a878f33bcb5d
jeff <jfroussel75@gmail.com>
2017-05-14 23:42:31 +0200

    refacto and implement passport for auth

--- app.js
+++ app.js
@@ -1,52 +1,86 @@
+// dependencies
 var express = require('express');
-var app = express();
 var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var expressLayouts = require('express-ejs-layouts');
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('layout extractScripts', true);
 app.set('layout extractStyles', true);
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(expressLayouts);
 
 
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/home', (req, res) => {
-  res.render('home');
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact');
-});
-
-app.get('/auth', (req, res) => {
-  res.render('auth');
-});
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/documentation', (req, res) => {
-  res.render('documentation');
-});
-
-app.get('/post/:id', (req, res) => {
-  res.render('post');
-});
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-var port = 3000;
-app.listen(port, function() {
-  console.log('Listening on http://localhost:%s/', port);
+app.use('/', routes);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/anodin1');
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
